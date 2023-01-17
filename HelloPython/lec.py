2cf72f5099f5377b462b034f2b312175af5bc16b
Lyudmila_Pavlova <mila_pavlova1991@mail.ru>
2023-01-17 23:07:56 +0500

    hello

---
+++ HelloPython/lec.py
@@ -0,0 +1,48 @@
+# print("Hello мир")
+# print("2Hello word")
+# print("I am Lyuda")
+
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+# s ='hello word'
+
+a = 123
+b =1.23
+s ='hello word'
+#print(s)  #вывод строки
+#print(a, '_' b, '_', s)
+#print('{1} - {2} - {0}'.format(a,b,s))
+#print(f'{a} - {b} - {s}')
+
+# f = True #логическая переменная
+# print(f)
+
+#list = ['1', '2', '3','hello word', 1,2,4.5,True]
+#col = ['hello word', 1,2,4.5,True]
+#print(list) #ВЫВОД данных от пользователя
+#print(col)
+
+#print('Введите a')
+#a = input()
+#print('Введите b')
+#b = input()
+#print(a, b, ' + ', b, ' = ', a + b)
+#print('{} {}'.format(a, b))
+#print(f'{a} {b}')
+
+#print('Введите a');
+#a = int(input()) #если целое число используем
+#print('Введите b');
+#b = int(input())
+#print(a, ' + ', b, ' = ', a + b)
+
+
+#print('Введите a');
+#a = float(input()) #если получение вещественных значений
+#print('Введите b');
+#b = float(input())
+#print(a, ' + ', b, ' = ', a + b)
